a2b00b1e0f210db7450d0ed06d9fff418085f953
varun040503 <129999153+varun040503@users.noreply.github.com>
2023-06-22 00:02:28 +0530

    Add files via upload

---
+++ calculator/calculator/build/precache-manifest.e423b0a5fdc9d2addc60a10aae6bf630.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "729bf6ddefe6113e7171",
+    "url": "/calculator/static/css/main.13de6a6c.chunk.css"
+  },
+  {
+    "revision": "729bf6ddefe6113e7171",
+    "url": "/calculator/static/js/main.729bf6dd.chunk.js"
+  },
+  {
+    "revision": "6640e213a06c46f75778",
+    "url": "/calculator/static/js/1.6640e213.chunk.js"
+  },
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "16ed34af5d77f80190a3a62fa65161e3",
+    "url": "/calculator/index.html"
+  }
+];
